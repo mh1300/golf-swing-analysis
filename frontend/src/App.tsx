f5ebbf1fa388415f091196897f4ec7476da175f4
matt <matthewhughes1300@gmail.com>
2025-06-26 17:47:40 -0400

    Create an experimental login screen shell

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,10 @@
 function App() {
 
   return (
-    <main className="py-10 h-screen bg-violet-50 space-y-5">
-      <h1>Test Text</h1>
+    <main className="bg-purple-50 h-screen">
+      <h1>
+        Test Text
+      </h1>
     </main>
   )
 }
