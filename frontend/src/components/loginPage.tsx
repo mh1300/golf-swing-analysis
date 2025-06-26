f5bdf8cf21d27582237c017758638fd88d59246e
matt <matthewhughes1300@gmail.com>
2025-06-26 19:35:00 -0400

    Rewrote code from scratch for practice

--- frontend/src/components/loginPage.tsx
+++ frontend/src/components/loginPage.tsx
@@ -3,53 +3,50 @@ import LoginForm from "./loginForm";
 import SignupForm from "./signupForm";
 import CreateOverlay from "./createOverlay";
 import LoginOverlay from "./loginOverlay";
+import { set } from "zod/v4-mini";
 
 export default function LoginPage() {
     const [isAnimated, setIsAnimated] = useState(false);
 
     return (
-        <div className="h-1/2 w-4/5 relative overflow-hidden rounded-lg">
+        <div
+            id="loginPage"
+            className="h-1/2 w-4/5 relative  rounded-lg"
+        >
             <div
-                id="login"
-                className={`p-10 bg-glass border-4 border-r-0 absolute top-0 left-0 h-full w-1/2 flex justify-center items-center transition-all duration-700 ease-in-out z-20 ${isAnimated ? "translate-x-full opacity-0" : ""}`}
+                id="loginForm"
+                className={`p-10 bg-glass absolute w-1/2 left-0 top-0 h-full border-6 border-r-0 flex justify-center items-center transition-all duration-700 ease-in-out ${isAnimated ? "translate-x-full opacity-0" : ""}`}
             >
                 <LoginForm />
             </div>
             <div
-                id="signup"
-                className={`p-10 bg-glass border-4 border-l-0 absolute top-0 left-0 h-full w-1/2 flex justify-center items-center transition-all duration-700 ease-in-out ${isAnimated
-                        ? "translate-x-full opacity-100 z-50"
-                        : "opacity-0 z-10"
-                    }`}
+                id="signupForm"
+                className={`p-10 bg-glass absolute w-1/2 left-0 top-0 h-full border-6 border-l-0 flex justify-center items-center transition-all duration-700 ease-in-out ${isAnimated ? "translate-x-full" : "opacity-0"}`}
             >
                 <SignupForm />
             </div>
             <div
                 id="overlay-container"
-                className={`absolute top-0 left-1/2 w-1/2 h-full overflow-hidden transition-transform duration-700 ease-in-out z-100 ${isAnimated ? "-translate-x-full" : ""
-                    }`}
+                className={`absolute w-1/2 h-full top-0 right-0 overflow-hidden transition-transform duration-700 ease-in-out ${isAnimated ? "-translate-x-full" : ""}`}
             >
                 <div
                     id="overlay"
-                    className={`bg-overlay relative -left-full h-full w-[200%] transform transition-transform duration-700 ease-in-out ${isAnimated ? "translate-x-1/2" : "translate-x-0"
-                        }`}
+                    className={`bg-overlay relative w-[200%] h-full top-0 right-full transition-transform duration-700 ease-in-out ${isAnimated ? "translate-x-1/2" : ""}`}
                 >
                     <div
-                        id="overlay-left"
-                        className={`w-1/2 h-full absolute flex justify-center items-center top-0 transform -translate-x-[20%] transition-transform duration-700 ease-in-out ${isAnimated ? "translate-x-0" : "-translate-x-[20%]"
-                            }`}
+                        id="createOverlay"
+                        className={`w-1/2 h-full absolute flex justify-center items-center top-0 right-0 transition-transform duration-700 ease-in-out ${isAnimated ? "translate-x-[20%]" : ""}`}
                     >
-                        <LoginOverlay
+                        <CreateOverlay
                             isAnimated={isAnimated}
                             setIsAnimated={setIsAnimated}
                         />
                     </div>
                     <div
-                        id="overlay-right"
-                        className={`w-1/2 h-full absolute flex justify-center items-center top-0 right-0 transform transition-transform duration-700 ease-in-out ${isAnimated ? "translate-x-[20%]" : "translate-x-0"
-                            }`}
+                        id="loginOverlay"
+                        className={`w-1/2 h-full absolute flex justify-center items-center top-0 left-0 transition-transform duration-700 ease-in-out ${isAnimated ? "" : "-translate-x-[20%]"}`}
                     >
-                        <CreateOverlay
+                        <LoginOverlay
                             isAnimated={isAnimated}
                             setIsAnimated={setIsAnimated}
                         />
