33632ee1355df624edee37c64df533202dcdf642
matt <matthewhughes1300@gmail.com>
2025-06-26 20:03:03 -0400

    Made a sentence lowercase

--- frontend/src/components/loginOverlay.tsx
+++ frontend/src/components/loginOverlay.tsx
@@ -9,7 +9,7 @@ interface LoginOverlayProps {
 export default function LoginOverlay(props: LoginOverlayProps) {
     return (
         <div className="text-center">
-            <h1 className="overlay-header py-10">Already Have an Account?</h1>
+            <h1 className="overlay-header py-10">Already have an account?</h1>
             <Button
                 className="py-3 px-6 bg-transparent rounded-full text-center text-white text-xl font-bold uppercase ring-2 ring-white active:scale-110 transition-transform ease-in"
                 onClick={() => {
