231b9163edbb0fbe882fa7b1a38397b77a9e2375
matt <matthewhughes1300@gmail.com>
2025-07-28 18:39:52 -0400

    Add minimum viable sidebar for video selection

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 import Dashboard from "./components/pages/dashboard";
 import LoginPage from "./components/pages/loginPage";
+import SwingBreakdown from './components/pages/swingBreakdown';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<LoginPage/>}/>
       <Route path="/dashboard" element={<Dashboard/>} />
+      <Route path="/swing/breakdown" element={<SwingBreakdown />} />
     </Routes>
 
   )
